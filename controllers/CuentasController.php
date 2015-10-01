513d331d56d6c797cc86d09111fdd0c33e8f3d58
jmcaro <jmcaro88@gmail.com>
2015-09-30 21:49:54 -0500

    controladores

---
+++ controllers/CuentasController.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use app\models\Cuentas;
+use app\models\CuentasSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use kartik\mpdf\Pdf;
+
+/**
+ * CuentasController implements the CRUD actions for Cuentas model.
+ */
+class CuentasController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Cuentas models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new CuentasSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Cuentas model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new Cuentas model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Cuentas();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id_cuenta]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Updates an existing Cuentas model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id_cuenta]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Cuentas model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+    
+    public function actionPdf($id) 
+    {
+        $model = $this->findModel($id);
+        $content = $this->renderPartial('_cuenta_de_cobro',['model'=>$model]);
+        $pdf = new Pdf([
+        // set to use core fonts only
+        'mode' => Pdf::MODE_UTF8,
+        // A4 paper format
+        'format' => Pdf::FORMAT_LETTER, 
+        // portrait orientation
+        'orientation' => Pdf::ORIENT_PORTRAIT, 
+        // stream to browser inline
+        'destination' => Pdf::DEST_BROWSER, 
+        // your html content input
+        'content' => $content,  
+        // format content from your own css file if needed or use the
+        // enhanced bootstrap css built by Krajee for mPDF formatting 
+        'cssFile' => 'css/cuenta.css',
+        // any css to be embedded if required
+        'cssInline' => '.kv-heading-1{font-size:18px}', 
+         // set mPDF properties on the fly
+        'options' => ['title' => 'Cuenta De Cobro'],
+         // call mPDF methods on the fly
+        'methods' => [ 
+            'SetHeader'=>['Elaborado desde areconsultoria.com.co || SIGER APP  '], 
+            'SetFooter'=>['{PAGENO}'],
+        ],
+        'filename' =>"CC ". strtoupper($model->idTercero->nombre). " - " . $model->id_cuenta,
+    ]);
+    
+    // return the pdf output as per the destination setting
+    return $pdf->render(); 
+    }
+
+    /**
+     * Finds the Cuentas model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Cuentas the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Cuentas::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
---
+++ controllers/EmpresaController.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use app\models\Empresa;
+use app\models\EmpresaSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\web\UploadedFile;
+
+/**
+ * EmpresaController implements the CRUD actions for Empresa model.
+ */
+class EmpresaController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Empresa models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new EmpresaSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Empresa model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new Empresa model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Empresa();
+
+        if ($model->load(Yii::$app->request->post())) {
+           
+            $image=UploadedFile::getInstance($model, 'image');
+            
+            $ext= end((explode(".", $image->name)));
+            $model->firma = $model->nit.".{$ext}";
+            $path = "uploads/". $model->firma;
+            if ($model->save()) {
+                $image->saveAs($path);
+                return $this->redirect(['view', 'id'=>$model->id_empresa]);
+            }
+            
+        } 
+
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+            
+        }
+    
+
+    /**
+     * Updates an existing Empresa model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post())) {
+            $image=UploadedFile::getInstance($model, 'image');
+            $ext= end((explode(".", $image->name)));
+            $model->firma = $model->nit.".".$image->extension;
+            $path = "uploads/". $model->firma;
+            if ($model->save()) {
+                $image->saveAs($path);
+                return $this->redirect(['view', 'id'=>$model->id_empresa]);
+            }
+            
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Empresa model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Empresa model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Empresa the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Empresa::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
