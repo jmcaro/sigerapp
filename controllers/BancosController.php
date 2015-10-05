48cacc42967a2649cf64e1faab3d9e47e5685ec6
jmcaro <jmcaro88@gmail.com>
2015-10-05 00:19:47 -0500

    add filter

--- controllers/BancosController.php
+++ controllers/BancosController.php
@@ -8,6 +8,7 @@ use app\models\BancosSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 
 /**
  * BancosController implements the CRUD actions for Bancos model.
